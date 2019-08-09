b03b2586e98f7ac1533dbe122c43862ef43ddc85
noecor <noeliacoronelg@hotmail.com>
2019-08-08 21:22:55 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-fetch('https://reqres.in/api/user')
+fetch('https://raw.githubusercontent.com/noecor/fetch/master/cv.json')
     .then(response => response.json())
-    .then(res => {
-        // console.log(res.data);
-        const {data} = res
-        data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
+    .then(response => {console.log(response);
     })
